46c99734bcf066297933fa2eaaa16befa5020733
NICO9 <nmmendez9@hotmail.com>
2024-10-12 16:15:57 +0400

    feat: menú, platos e ingredientes creados en un diccionario, inicio de creación de una función

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# Menu_para_invitados
-Menú para invitados
+# Menú para invitados (Nicolás Mendez XXII)
+
+Creo una estructura de datos para el menú y los diferentes platos donde, para cada plato, indico los ingredientes necesarios para preparar la receta.
+
+Luego, creo una función que muestre la lista de la compra (ingredientes).
+
+## Primer paso: Crear menú en un diccionario
+
+### MENÚ:
+
+- Aperitivo: Guacamole.
+    - Ingredientes:
+        - 5 ramitas de cilantro fresco (solo las hojas)
+        - 100 g de cebolla cortada en cuartos
+        - 1 chile fresco (no demasiado picante) cortado por la mitad y sin semillas
+        - 1 diente de ajo
+        - 1 tomate (100-120 g) cortado en cuartos
+        - 450 g de aguacates maduros pelados y sin hueso
+        - 1 cucharada de zumo de limón
+        - 1 cucharadita de sal
+
+- Primer plato: Pimientos del piquillo rellenos
+    - Ingredientes:
+        - 1 huevo duro
+        - 100 g de palitos de cangrejo
+        - 150 g de atún en aceite de oliva
+        - 50 g de mayonesa
+        - 12 pimientos del piquillo
+        - 50 g de pimiento verde
+        - 50 g de pimiento rojo
+        - 50 g de cebolla
+        - 25 g de vinagre de vino
+        - ½ cucharadita de sal
+        - 50 g de aceite de oliva - virgen extra
+
+- Segundo plato: Solomillo de cerdo agridulce
+    - Ingredientes:
+        - 60 g de aceite de oliva
+        - 3 dientes de ajo
+        - 500 g de solomillo de cerdo en trozos de 3 cm salpimentados
+        - 150 g de cebolla en trozos
+        - 1 manzana (sin piel ni semillas) en cuartos cortados por la mitad
+        - 1 cucharada de tomate concentrado
+        - 600 g de calabacines en espirales
+        - 30 g de brandy
+        - 100 g de vino de Oporto
+        - 30 g de ciruelas pasas sin hueso
+        - 30 g de orejones de albaricoque
+        - 1 cucharadita de mostaza
+        - 1 - 2 cucharadas de piñones tostados
+
+- Postre: Brownie de chocolate con helado de turrón
+    - Ingredientes:
+        - 250 g de chocolate amargo 70% cacao, en trozos
+        - 220 g mantequilla sin sal, en cubos y un poco más para engrasar
+        - 380 g de azúcar
+        - 6 huevos
+        - 1 pizca de sal fina
+        - 150 g de harina de trigo
+        - 150 g de nueces, en trozos
+
+## Segundo paso: Crear una función
+
+Esta función toma como parámetro de entrada el menú e imprime el menú con todos sus platos e ingredientes necesarios para su compra / preparación.
---
+++ menu_para_invitados.py
@@ -0,0 +1,56 @@
+menu = {
+    "- Aperitivo: Guacamole.": ["cilantro fresco",
+                                "cebolla",
+                                "chile fresco",
+                                "ajo",
+                                "tomate",
+                                "aguacate maduros",
+                                "zumo de limón",
+                                "sal"],
+    "- Primer plato: Pimientos del piquillo rellenos.": ["huevo",
+                                                         "palitos de cangrejo",
+                                                         "atun en aceite de oliva",
+                                                         "mayonesa",
+                                                         "pimientos del piquillo",
+                                                         "pimiento verde",
+                                                         "pimiento rojo",
+                                                         "cebolla",
+                                                         "vinagre de vino",
+                                                         "sal",
+                                                         "aceite de oliva"],
+
+    "- Segundo plato: Solomillo de cerdo agridulce.": ["aceite de oliva",
+                                                       "ajo",
+                                                       "solomillo de cerdo",
+                                                       "cebolla",
+                                                       "manzana",
+                                                       "tomate concentrado",
+                                                       "calabacines en espirales",
+                                                       "brandy",
+                                                       "vino oporto",
+                                                       "ciruelas pasas sin hueso",
+                                                       "orejones de albaricoque",
+                                                       "mostaza",
+                                                       "piñones tostados"],
+
+    "- Postre: Brownie de chocolate con helado de turrón.": ["chocolate amargo 70porciento cacao",
+                                                             "mantequilla sin sal",
+                                                             "azúcar",
+                                                             "huevo",
+                                                             "sal",
+                                                             "harina de trigo",
+                                                             "nueces"],
+}
+
+def lista_ingredientes(menu):
+    """
+    Recibe el menú.
+    Devuelve la lista de ingredientes necesarios que hay que comprar para
+    la elaboración de cada plato.
+    """
+    lista = []
+
+    for plato in menu:
+        lista.append(plato)
+
+    return lista
