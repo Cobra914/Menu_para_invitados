39486efdd1a53efc467948dccbde522d3a2c6868
NICO9 <nmmendez9@hotmail.com>
2024-10-13 16:33:52 +0400

    feat: Imprime los ingredientes que hay que comprar sin repetir (si hay repetidos). Empiezo a crear diccionario con cantidades para cada ingrediente.

--- menu_para_invitados.py
+++ menu_para_invitados.py
@@ -2,7 +2,7 @@ menu = {
     "- Aperitivo: Guacamole.": ["cilantro fresco",
                                 "cebolla",
                                 "chile fresco",
-                                "ajo",
+                                "diente de ajo",
                                 "tomate",
                                 "aguacate maduros",
                                 "zumo de limón",
@@ -20,7 +20,7 @@ menu = {
                                                          "aceite de oliva"],
 
     "- Segundo plato: Solomillo de cerdo agridulce.": ["aceite de oliva",
-                                                       "ajo",
+                                                       "diente de ajo",
                                                        "solomillo de cerdo",
                                                        "cebolla",
                                                        "manzana",
@@ -42,19 +42,62 @@ menu = {
                                                              "nueces"],
 }
 
+cantidades_gramos_unid = {
+    "cilantro fresco": 5,
+    "cebolla": 100,
+    "chile fresco": 1,
+    "diente de ajo": 1,
+    "tomate": 120,
+    "aguacate maduros": 450,
+    "zumo de limón": 1,
+    "sal": 1,
+    "huevo": 1,
+    "palitos de cangrejo": 100,
+    "atun en aceite de oliva": 150,
+    "mayonesa": 50,
+    "pimientos del piquillo": 12,
+    "pimiento verde": 50,
+    "pimiento rojo": 50,
+    "cebolla": 50,
+    "vinagre de vino": 25,
+    "sal": 0.5,
+    "aceite de oliva": 50,
+    "aceite de oliva": 60,
+    "diente de ajo": 3,
+    "solomillo de cerdo": 500,
+    "cebolla": 150,
+    "manzana": 1,
+    "tomate concentrado": 1,
+    "calabacines en espirales": 600,
+    "brandy": 30,
+    "vino oporto": 100,
+    "ciruelas pasas sin hueso": 30,
+    "orejones de albaricoque": 30,
+    "mostaza": 1,
+    "piñones tostados": 2,
+    "chocolate amargo 70porciento cacao": 250,
+    "mantequilla sin sal": 220,
+    "azúcar": 380,
+    "huevo": 6,
+    "sal": 1,
+    "harina de trigo": 150,
+    "nueces":150,
+}
+
+
 def lista_ingredientes(menu):
     """
     Recibe el menú.
     Devuelve la lista de ingredientes necesarios que hay que comprar para
     la elaboración de cada plato.
     """
-    lista = []
-
+    nueva_lista = []
+    
     for plato in menu:
         for ingrediente in menu[plato]:
-            if ingrediente not in lista:
-                lista.append(ingrediente)
+            if ingrediente not in nueva_lista:
+                nueva_lista.append(ingrediente)
         
-    return lista
+    return nueva_lista
 
 print(lista_ingredientes(menu))
